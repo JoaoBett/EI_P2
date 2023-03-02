be63df8eeb1112d7c4f3618c0681ce4c14672693
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-03-02 11:57:07 +0000

    refactor: variables

--- ficha0/FichaPratica0/src/Main.java
+++ ficha0/FichaPratica0/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int x = 5;
 
         //variavel do tipo double
-        double soma = 0;
+        double sum = 0;
 
         //variavel do tipo boolean
         boolean flag = false;
@@ -18,10 +18,10 @@ public class Main {
             flag = false;
         }
 
-        int verificacao;
-        verificacao = x % 2;
+        int verification;
+        verification = x % 2;
 
-        switch (verificacao){
+        switch (verification){
             case 0:
                 flag = false;
                 break;
@@ -39,26 +39,26 @@ public class Main {
 
         //estrutura de repetição for
         for(int i = 1; i <= 1; i++){
-            soma += i;
+            sum += i;
         }
 
         //reiniciar o valor da soma para 0
-        soma = 0;
+        sum = 0;
 
         //estrutura de repetição while
         int i= 1;
-        while(i <= soma){
-            soma = 1 + x;
+        while(i <= sum){
+            sum = 1 + x;
         }
 
         //reiniciar o valor da soma e do i
-        soma = 0;
+        sum = 0;
         i = 1;
 
         //estrutura de repetição do while
         if(i <= x) {
             do {
-                soma += i;
+                sum += i;
                 i++;
             } while (i <= x);
         }
