8e534890720563456e09bca8b8675260662b9a3f
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2024-03-07 16:12:04 +0000

    feat: classes

---
+++ 2023-2024/TrabalhoLaboratorial/src/Aluno.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class Aluno {
+    String nome;
+    long numero;
+    LinkedList<Aula> aulas;
+    public static void preencherSumario(LinkedList<Aula> aulas){
+
+    };
+}
---
+++ 2023-2024/TrabalhoLaboratorial/src/Professor.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Professor {
+    String nome;
+    long numero;
+    LinkedList<Aula> aulas;
+
+    public static void preencherSumario(LinkedList<Aula> aulas){
+
+    };
+}
